1165502f8fc481b95c7a5025112e5b7933fbb925
Nazlul Rizan <eng.nazrizan@gmail.com>
2025-07-23 14:04:46 +0530

    updated sdk import

--- src/app/ClientLayout.tsx
+++ src/app/ClientLayout.tsx
@@ -1,15 +1,11 @@
 "use client";
 import { PrivyProvider } from "@privy-io/react-auth";
 import { useEffect } from "react";
+import { sdk } from "@farcaster/miniapp-sdk";
 
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
   useEffect(() => {
-    if (
-      typeof window !== "undefined" &&
-      window.privy?.actions?.ready
-    ) {
-      window.privy.actions.ready();
-    }
+    sdk.actions.ready();
   }, []);
 
   return (
